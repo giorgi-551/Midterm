55560b702989da18485041c8151d2feb4adf701a
giorgi-551 <maisuraze16@gmail.com>
2025-04-24 11:31:58 +0400

    Add files via upload

---
+++ path_to_your_project/giorgi_maisuradze_18/midterm1/t2/mixer/composer/A22.java
@@ -0,0 +1,13 @@
+package giorgi_maisuradze_18.midterm1.t2.mixer.composer;
+
+public class A22 {
+    private int month = 7; // July (for example)
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+}
