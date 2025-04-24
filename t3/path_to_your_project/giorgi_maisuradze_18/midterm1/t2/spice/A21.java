b393bb7ee10fa0ad0e7e5cd92ae74fc4ba83b3a1
giorgi-551 <maisuraze16@gmail.com>
2025-04-24 11:29:49 +0400

    Add files via upload

---
+++ t3/path_to_your_project/giorgi_maisuradze_18/midterm1/t2/spice/A21.java
@@ -0,0 +1,22 @@
+package giorgi_maisuradze_18.midterm1.t2.spice;
+
+public class A21 {
+    private int width = 23;
+    private int height = 33;
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+}
